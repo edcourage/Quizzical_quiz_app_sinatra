42f3206d76f5729f5aa89c781a09650bcf7c9506
Ed Courage <edcourage@live.com>
2020-01-28 16:57:03 +0000

    question nop longer repeat, needs refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,21 @@
 class Game
   def initialize(question = Question)
     @question = question
+    @questionTracker = []
   end
 
   def new_question
-    @question.random_question
+    question = @question.random_question
+    if @questionTracker.include?(question)
+      if @questionTracker.length == @question.all.length
+        while @questionTracker.include?(question) do
+          question = @question.random_question
+        end
+      else
+        return nil
+      end
+    end
+    @questionTracker << question
+    question
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,8 +8,20 @@ describe Game do
 
   context "#new_question" do
     it "returns a question" do
-      allow(question).to receive(:random_question) {["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]]}
+      allow(question).to receive(:random_question) { ["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]] }
       expect(game.new_question).to eq ["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]]
+    end
+
+    it "don't repeat the same question" do
+      allow(question).to receive(:all) { [["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]],
+        ["What is an axolotl?","A species of salamander",["A nerve in the brain","A multi-axled vehicle","A type of mortice lock","A species of salamander"]]] }
+
+      allow(question).to receive(:random_question) { ["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]] }
+      expect(game.new_question).to eq ["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]]
+      allow(question).to receive(:random_question) { ["The Plaka is the oldest quarter of which city?","Athens",["Athens", "Prague","Rome","Vienna"]] }
+      expect(game.new_question).to eq nil
+      allow(question).to receive(:random_question) { ["What is an axolotl?","A species of salamander",["A nerve in the brain","A multi-axled vehicle","A type of mortice lock","A species of salamander"]] }
+      expect(game.new_question).to eq ["What is an axolotl?","A species of salamander",["A nerve in the brain","A multi-axled vehicle","A type of mortice lock","A species of salamander"]]
     end
 
 
