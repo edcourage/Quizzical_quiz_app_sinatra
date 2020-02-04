41cb4c556f17de5fee29ab46d9589f6861515782
Ed Courage <edcourage@live.com>
2020-02-04 17:25:23 +0000

    change to datamapper set up

--- db/data_mapper_setup.rb
+++ db/data_mapper_setup.rb
@@ -5,7 +5,7 @@ require 'dm-postgres-adapter'
 require_relative '../lib/question'
 
 
-DataMapper.setup(:default, "postgres://localhost/quizzical_#{ ENV['RACK_ENV'] == nil ? 'development' : ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/quizzical_#{ ENV['RACK_ENV'] == nil ? 'development' : ENV['RACK_ENV']}")
 
 #This checks the models for validity and initializes all properties associated with relationships.
 DataMapper.finalize
