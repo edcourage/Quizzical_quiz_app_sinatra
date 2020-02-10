d095cf6d408b1d6a3073b4054a3f2c0b482b5ab3
Ed Courage <edcourage@live.com>
2020-02-10 18:14:07 +0000

    code clean up

--- app.rb
+++ app.rb
@@ -15,23 +15,24 @@ class Quizzical < Sinatra::Base
   get '/questions' do
     session[:game_id] = Game.game_id if session[:game_id].nil?
     Game.create(session[:game_id]) if Game.instance(session[:game_id]).nil?
-    @game_id = session[:game_id]
+    @game = Game.instance(session[:game_id])
     @question = Game.instance(session[:game_id]).new_question
     erb :questions
   end
 
   post '/questions' do
+    @game = Game.instance(session[:game_id])
     if params[:selected_answer] == params[:correct_answer]
       @result = "Correct!"
-      Game.instance(session[:game_id]).question_number += 1
+      @game.question_number += 1
       redirect '/result' if Game.instance(session[:game_id]).question_number > 10
     elsif params[:selected_answer] == "Pass"
-      Game.instance(session[:game_id]).passes_remaining -= 1
+      @game.passes_remaining -= 1
     else
       @result = "Wrong! Should have been #{ params[:correct_answer] }"
-      Game.instance(session[:game_id]).question_number = 1
-      Game.instance(session[:game_id]).lives_remaining -= 1
-      redirect '/game-over' if Game.instance(session[:game_id]).lives_remaining == 0
+      @game.question_number = 1
+      @game.lives_remaining -= 1
+      redirect '/game-over' if @game.lives_remaining == 0
     end
     redirect "/questions?result=#{@result}"
   end
