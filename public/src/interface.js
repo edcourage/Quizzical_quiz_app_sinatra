f81418e9347e389af98373dd49f1a8c6e8b2c852
Ed Courage <edcourage@live.com>
2020-02-11 17:24:29 +0000

    colour now added to signify if correct answer has beed chosen. previous answer outcome removed

--- public/src/interface.js
+++ public/src/interface.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
    $("#howToPlayIndexButton").hide()
  });
 
- document.getElementById("questionForm")
+
 
 });
--- public/src/questionPageDom.js
+++ public/src/questionPageDom.js
@@ -1,10 +1,15 @@
 function PreSubmit(selectedAnswer, correctAnswer){
   form = document.getElementById("questionForm")
+
+  if (selectedAnswer == correctAnswer) {
+    document.getElementById(selectedAnswer).style.color = "#73e600";
+  } else if (selectedAnswer != correctAnswer && selectedAnswer != 'Pass') {
+    document.getElementById(selectedAnswer).style.color = "red";
+  }
   selectedInput = document.createElement("INPUT");
   selectedInput.setAttribute("type", "hidden")
   selectedInput.setAttribute("name", "selected_answer")
   selectedInput.setAttribute("value", selectedAnswer)
-
   correctAnswerInput = document.createElement("INPUT");
   correctAnswerInput.setAttribute("type", "hidden")
   correctAnswerInput.setAttribute("name", "correct_answer")
